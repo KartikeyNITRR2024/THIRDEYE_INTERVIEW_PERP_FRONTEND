30545bd41d8c2fe0c5bfb6731c0b3804bb0d178d
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2026-06-26 17:18:25 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/src/logo.png',
+            src: '/src/assets/logo.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/src/logo.png',
+            src: '/src/assets/logo.png',
             sizes: '512x512',
             type: 'image/png'
           }
